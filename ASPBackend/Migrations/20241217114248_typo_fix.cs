93844df0a20ce80344c9c22bb77c039855813e78
Swann <swann.rouanet@gmail.com>
2024-12-17 13:18:04 +0100

    feat: add migrations

---
+++ ASPBackend/Migrations/20241217114248_typo_fix.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASPBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class typo_fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "gems",
+                table: "Users",
+                newName: "Gems");
+
+            migrationBuilder.RenameColumn(
+                name: "coins",
+                table: "Users",
+                newName: "Coins");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Gems",
+                table: "Users",
+                newName: "gems");
+
+            migrationBuilder.RenameColumn(
+                name: "Coins",
+                table: "Users",
+                newName: "coins");
+        }
+    }
+}
