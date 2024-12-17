9a71509a00b4669eb0e3a1c04c2c7eb9ac1cf2b7
Swann <swann.rouanet@gmail.com>
2024-12-17 13:17:37 +0100

    feat: load db url from env and connect to db

--- ASPBackend/Program.cs
+++ ASPBackend/Program.cs
@@ -1,10 +1,23 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+
+using ASPBackend.Data;
+
+// Initialize the application
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+// Load .env file
+Env.Load();
+var dbURl = Env.GetString("DB_URL");
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+services.AddDbContext<AppDbContext>(options => options.UseNpgsql(dbURl));
 
+// Build the application
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
